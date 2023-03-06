b36b9002df866bcdc085099ab111fdac116c21ad
haobayuxi <whaohit@gmail.com>
2023-03-06 12:24:28 +0800

    update

---
+++ src/server/src/client.rs
@@ -0,0 +1,12 @@
+
+
+struct Client {
+    id: i32,
+    txn_id: u64,
+}
+
+impl Client {
+    pub async fn run_transaction(&mut self) {
+        
+    }
+}
--- src/server/src/cto.rs
+++ src/server/src/cto.rs
@@ -11,3 +11,16 @@ impl CTO {
         }
     }
 }
+
+pub async fn run_propose_server(propose_server: ProposeServer) {
+    let addr = propose_server.addr_to_listen.parse().unwrap();
+
+    tracing::info!("propose server listening on: {:?}", addr);
+
+    let server = ClientServiceServer::new(propose_server);
+
+    match Server::builder().add_service(server).serve(addr).await {
+        Ok(_) => tracing::info!("propose rpc server start done"),
+        Err(e) => panic!("propose rpc server start fail {}", e),
+    }
+}
