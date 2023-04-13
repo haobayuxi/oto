18bd942502d27471e59a0e08624da34fbbcbfbd8
haobayuxi <whaohit@gmail.com>
2023-04-13 18:25:09 +0800

    update

--- src/workload/src/micro_txn.rs
+++ src/workload/src/micro_txn.rs
@@ -51,9 +51,10 @@ async fn run_transaction(
     //     .map(|f| Arc::new(RwLock::new(f)))
     //     .collect();
     let (status, result) = coordinator.tx_exe().await;
-    if !status {
-        coordinator.tx_abort().await;
-        return false;
-    }
-    return coordinator.tx_commit().await;
+    // if !status {
+    //     coordinator.tx_abort().await;
+    //     return false;
+    // }
+    // return coordinator.tx_commit().await;
+    return true;
 }
