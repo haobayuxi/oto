816743a1e427e72867882ce658b4858a17d15112
haobayuxi <whaohit@gmail.com>
2023-04-26 15:30:01 +0800

    update

---
+++ cp.sh
@@ -0,0 +1,16 @@
+#!/yuxi/bash
+
+cp target/release/*server ~/server0
+
+
+cp target/release/*server ~/server1
+
+cp target/release/*server ~/server2
+
+# cp run_client.py ~/yuxi
+#cp aggregate_result.py ~/yuxi
+cp clean.sh ~/yuxi
+
+cp target/release/*client ~/client0
+cp target/release/*client ~/client1
+cp target/release/*client ~/client2
