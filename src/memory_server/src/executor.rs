2e0d25dec9af0e60932684cedc69c0ca5ffcf7ea
haobayuxi <whaohit@gmail.com>
2023-06-16 15:57:49 +0800

    update

--- src/memory_server/src/executor.rs
+++ src/memory_server/src/executor.rs
@@ -90,7 +90,7 @@ impl Executor {
                                     // init node
                                     let txn_id = coor_msg.msg.txn_id;
                                     let (client_id, index) = get_txnid(txn_id);
-                                    let mut last_index = TXNS[client_id as usize].len();
+                                    let mut last_index = TXNS[client_id as usize].len() - 1;
                                     while index > last_index as u64 {
                                         let node = Node::default();
                                         TXNS[client_id as usize].push(node);
