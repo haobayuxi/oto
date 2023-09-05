f42753d220cc1702bd381cab5ddfe0daf9c401e8
haobayuxi <whaohit@gmail.com>
2023-09-05 15:20:08 +0800

    update

--- src/memory_server/src/executor.rs
+++ src/memory_server/src/executor.rs
@@ -82,13 +82,11 @@ impl Executor {
                                 let read_set = coor_msg.msg.read_set.clone();
                                 // need wait
                                 let local_clock = get_currenttime_millis();
-                                // println!(
-                                //     "ts={},
-                                //     committs={},
-                                //     localclock = {}
-                                // ",
-                                //     ts, MAX_COMMIT_TS, local_clock
-                                // );
+                                println!(
+                                    "ts={},committs={},localclock = {}
+                                ",
+                                    ts, MAX_COMMIT_TS, local_clock
+                                );
                                 if self.dtx_type == DtxType::spanner {
                                     if ts > MAX_COMMIT_TS {
                                         // wait
