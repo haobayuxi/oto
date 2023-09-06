f2c54490f2b2e68afdede8a57370db61954cf835
haobayuxi <whaohit@gmail.com>
2023-09-06 16:16:15 +0800

    update

--- src/server/src/server.rs
+++ src/server/src/server.rs
@@ -12,10 +12,11 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let server_config: ConfigInFile = serde_yaml::from_reader(f).unwrap();
     let db_type: common::DbType = serde_yaml::from_str(&server_config.db_type).unwrap();
     let dtx_type = serde_yaml::from_str(&server_config.dtx_type).unwrap();
+    let config = Config::default();
     let mut server = DataServer::new(
         id,
-        Config::default(),
-        server_config.client_num * 3,
+        config.clone(),
+        server_config.client_num * (config.client_addr.len() as u64),
         dtx_type,
     );
     server.init_and_run(db_type, dtx_type).await;
