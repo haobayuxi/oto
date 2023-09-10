4b45f9e448c7340c7e1ce0b130f78c816f183983
haobayuxi <whaohit@gmail.com>
2023-09-10 20:04:20 +0800

    update

--- src/memory_server/src/executor.rs
+++ src/memory_server/src/executor.rs
@@ -235,9 +235,9 @@ impl Executor {
                                 let txn_id = coor_msg.msg.txn_id;
                                 let (client_id, index) = get_txnid(txn_id);
 
-                                let node: &mut Node = &mut TXNS[client_id as usize][index as usize];
-                                node.txn = Some(coor_msg.msg);
-                                node.committed = true;
+                                // let node: &mut Node = &mut TXNS[client_id as usize][index as usize];
+                                // node.txn = Some(coor_msg.msg);
+                                // node.committed = true;
                                 coor_msg.call_back.send(reply);
                                 // node.callback = Some(coor_msg.call_back);
                                 // send commit txn to dep_graph
